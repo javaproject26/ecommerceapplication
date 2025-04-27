b64ed1090154ce2b454d6e746b60685de2515a13
javaclasses26 <javaclasses26@gmail.com>
2025-04-27 12:19:15 +0530

    Add download script for JAR

---
+++ download-jars.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Downloading JAR file..."
+curl -L -o demo-0.0.1-SNAPSHOT.jar "https://drive.google.com/uc?export=download&id=1KDHQsOtiQYqhpkhZoP7BUAypoM-9Iol8"
+echo "Download complete."
